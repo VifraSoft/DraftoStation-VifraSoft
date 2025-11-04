dae201e3a372c9418dc1fb83777a126c37a670e9
VifraSoft <vifrasoftbusiness@gmail.com>
2025-11-04 15:58:38 -0300

    We added the base layout with the initial structure

--- routes/web.php
+++ routes/web.php
@@ -5,27 +5,21 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');;
 
 Route::get('/login', function () {
     return view('authentication.login');
-});
+})->name('login');;
 
 
 Route::get('/register', function () {
     return view('authentication.register');
-});
+})->name('register');;
  
 Route::get('/game', function () {
     return view('game');
 });
-<<<<<<< Updated upstream
-=======
-
-
 
 Route::resource('users', UserController::class);
-
 Route::post('/login', [UserController::class, 'login'])->name('users.login');
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
->>>>>>> Stashed changes
